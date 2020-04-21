89559d64a401c109874de496075c642e2e74b0ff
austinogiza <44683018+austinogiza@users.noreply.github.com>
2020-04-21 13:02:17 +0100

    add

---
+++ number.py
@@ -0,0 +1,57 @@
+
+import random
+
+easyNumber = random.randint(1, 10)
+mediumNumber = random.randint(1, 20)
+hardNumber = random.randint(1, 50)
+answer = ''
+number = 2
+countEasyLimt = 6
+countMediumLimit = 4
+countHardLimit = 3
+count = 0
+guess = ''
+
+while True:
+    answer = str(input("Enter a level you want: ")).lower()
+    if answer == 'easy':
+        while count < countEasyLimt:
+            guess = int(input('Guess a number between 1 - 10: '))
+            count += 1
+            if guess == easyNumber:
+                print("You won")
+                break
+            elif guess > easyNumber:
+                print("Guess is too high, choose a number between 1-10")
+            else:
+                print("You failed")
+    elif answer == 'medium':
+        while count < countMediumLimit:
+            guess = int(input('Guess a number between 1 - 20: '))
+            count += 1
+            if guess == mediumNumber:
+                print("You won")
+                break
+            elif guess > mediumNumber:
+                print("Guess is too high, choose a number between 1-20")
+            else:
+                print("You failed")
+    elif answer == 'hard':
+        while count < countHardLimit:
+            guess = int(input('Guess a number between 1 - 100: '))
+            count += 1
+            if guess == hardNumber:
+                print("You won")
+                break
+            elif guess > hardNumber:
+                print("Guess is too high, choose a number between 1-100")
+            else:
+                print("You failed")
+    elif answer == 'help':
+        print("""we have 3 levels, 
+            'Easy'
+            'Medium'
+            'Hard'
+              """)
+    else:
+        break
